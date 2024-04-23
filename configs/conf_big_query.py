a4a99b7b2b90574bd9581b47707ca961d5fc4134
Jose Cardoso <josewelkson@gmail.com>
2024-04-23 20:40:17 -0300

    Subida funcional!

---
+++ configs/conf_big_query.py
@@ -0,0 +1,13 @@
+credential_big_query = {
+  "type": "service_account",
+  "project_id": "projectbot-421014",
+  "private_key_id": "a560d614b33e5cdb32228423681381d80fdf20a1",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQC6mwwCZS+OqkR0\n+58FWGiJoa6rYiHck6SYAi2NVrddkIrJepQmPkT+Ed7DZbgtBhwggKNy5GVL3U8Q\nOsJdJYp4GdcVY0NraXT06GSrJNjMYYeMdZ2sk0ZA4qxC0kE2pnchky+XzZAryvfH\nF/6U9IRH7F3GhkmLmcZsj9MqL/d++fRtbxoQY5FsERgDsqZoIr9ZS/H6fsCNUNel\n2evGdMGxbNsvPVg2dZtNtXcryukHfEuH6ChBa2IU994xDKr8UQkepRRS27TwgheY\n6hPM0r2pdXOPaO0Gyt9yo0VyuJ7hd48lAAQy0EVj8yC6NFfhyNA74cKKyMXVciIb\na5+tx0MtAgMBAAECggEAA5WV1KKdQr1vhNsOchEBf7Sc1nWTQUQ5aE1maELEUTua\nXDC1Zsn8aPBD70yQDewCSfeJQYbfO+amLaF1+Um5eqwYZF9HPL7GUdai6bdSo2kI\n1wBYBSgWXzRbawnUhIMMYmyMK1DLVk6sQA/sbRpd7hm9Wrev8vVYXokhXRFAd5kM\ntgI2CNC9BcctUPgDRFmHiWX6fMeFXPPYzzAkcWAxoMSeO0zU81xyL8B2UolpNMil\nFNG3PXS0skZ83OYWhSCWBEF4g4e14FFZegZHaClot2gWa1FSpkb6WIXWxB/jFj67\nBoKX81vOP9G7gyMgsipRYK7XDT276PXk6MkXELCcYQKBgQD4T163iZWXvbngI4fr\n6TseeGvOauLV7baqiA0NEYFET6/YQgtK/cYbrmZziOFk3yuq1iRRuMjIii3qZ6T6\nKXgvG5IorJ0xvuTRIvqcU8jdNAB4Yj7kVd+LEQcTlmSFz8EIiZ77gS+LHJLRoHar\ngkdy8TwhkzWdF9MMddakAi33cQKBgQDAYnpCqTgYjhEsqTnUweud+m2rOClK4yTR\nBRFPD6g1+asMOuawR3v4YS9XoB6LZ7FarVUPnCKV9AaPapSDzYESPXm7wdse9svr\nyVR79Xdg64PcsPZAOQLLAm7Aj6Ep0dw6gBIAZkwsrc6ZjtDKGywuNoDxHAU3Cclp\nFzphoj4BfQKBgCZBjDU4xExhla6bqshE4a/bG7jsrNAK1LbHSUN3K/RLVqs52aWP\nHaOMJgqszxUMcRzb6zKQBpcm+tX/a+KC2xpe5BDSL1HHuFckTXoiSnMSHPRvJoNl\nW5CAahexdwFKtdHL6iuThrCnEVOspjvcUYe69OmzVXJDhmeIDC6ui0hxAoGARubE\nOjcVNm99QuFnL9L8G+/EZTrAdRn7YpEkjOevTZ0i/dHjXQorVSG0Ce4385aWlV2W\nILehQ3R4yng0HdMqA2fmqlzKfcHk65P6b+jKQ0zo/ve0+XxJXDocSRlmAvyV1nWG\nrOEGp9yybINmzWVwRePOZ421GlCgCvwFhx/9DB0CgYEA4aBRf2TKgvIpyZLJv4Y8\nhZUTUPuLXJLLKrXtdHfGgY6kZRCYxwl8EEA9fhBi9Ac3KHRO4Osy4afTc/Hrd5/p\niTggJUjetHsjMdSGG/HopcNQBrN7c+VA1W1FUzfGJGcGAEL4WaVOq1YHA2WhUMnq\nSe81Gv/Vhs6qQ/1czL3jyZ4=\n-----END PRIVATE KEY-----\n",
+  "client_email": "projectbot@projectbot-421014.iam.gserviceaccount.com",
+  "client_id": "100091812926786171098",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/projectbot%40projectbot-421014.iam.gserviceaccount.com",
+  "universe_domain": "googleapis.com"
+}
